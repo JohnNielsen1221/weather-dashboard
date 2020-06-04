c4fcad11dae91affc804d7febd7f0e6948c1ff8d
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-04 15:15:26 -0600

    started created search and history js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,1 +1,28 @@
 var apiKey = '9b8ee1ceace92b1727eafef8733c9a30';
+var searchEl = document.getElementById('search-button');
+var inputEl = document.getElementById('city-input');
+var historyEl = document.getElementById('history');
+var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
+
+searchEl.addEventListener("click",function() {
+    var searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
+});
+
+function renderSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i=0; i<searchHistory.length; i++) {
+        const historyItem = document.createElement("input");
+        historyItem.setAttribute("type","text");
+        historyItem.setAttribute("readonly",true);
+        historyItem.setAttribute("class", "form-control d-block bg-white");
+        historyItem.setAttribute("value", searchHistory[i]);
+        historyItem.addEventListener("click",function() {
+            getWeather(historyItem.value);
+        })
+        historyEl.append(historyItem);
+    }
+}
